c21bd19320acff790c409144001500b11c4c91e9
Julio Seijas <jus@microsoft.com>
2022-12-12 17:25:47 +0100

    sqlite

--- config.py
+++ config.py
@@ -16,12 +16,7 @@ class ProductionConfig(Config):
     )
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.getenv('DBUSER'),
-    dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST'),
-    dbname=os.getenv('DBNAME')
-    )
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     DEBUG = True
 
 class GithubCIConfig(Config):
